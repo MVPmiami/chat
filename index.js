005619dbe4d2e68f787055bf20579c2622fbe4ea
Aliaksei Siankevich <asenkevich1994@gmail.com>
2022-10-19 10:47:32 +0300

    asdasdad

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require('express')
 const app = express()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
-const Discord = require('discord.js')
+/*const Discord = require('discord.js')
 const webhook = new Discord.WebhookClient({
   id: '1032186334607056986',
   token: 'aaWd39Z7olcW1nk4i2RQnZM9KZT1lukhtdMVfTus3T1Vlc7aNLhdSH0M8sGl8qDISM_3',
-})
-process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
-webhook.send('hi')
+})*/
+//process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
+//webhook.send('hi')
 
 app.use(express.static(__dirname + `/assets`))
 
@@ -22,7 +22,7 @@ io.on('connection', (socket) => {
       message: data.message,
       name: data.name,
     })
-    webhook.send(`${data.name} : ${data.message}`)
+    //webhook.send(`${data.name} : ${data.message}`)
   })
 })
 
