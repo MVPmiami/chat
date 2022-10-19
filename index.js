a3fda62b8f2e05511aa90053e13fd7fb1c01b2f4
Aliaksei Siankevich <asenkevich1994@gmail.com>
2022-10-19 11:02:29 +0300

    asdasdad

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const express = require('express')
 const app = express()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
-/*const Discord = require('discord.js')
+const Discord = require('discord.js')
 const webhook = new Discord.WebhookClient({
   id: '1032186334607056986',
   token: 'aaWd39Z7olcW1nk4i2RQnZM9KZT1lukhtdMVfTus3T1Vlc7aNLhdSH0M8sGl8qDISM_3',
-})*/
-//process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
-//webhook.send('hi')
-
+})
+require('dotenv').config()
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
+webhook.send('hi')
+const PORT = process.env.PORT || 5000
 app.use(express.static(__dirname + `/assets`))
 
 app.use('/', (req, res) => {
@@ -26,6 +27,6 @@ io.on('connection', (socket) => {
   })
 })
 
-http.listen(3000, () => {
-  console.log('server running on 3000 port lol kek')
+http.listen(PORT, () => {
+  console.log(`server running on ${PORT} port`)
 })
